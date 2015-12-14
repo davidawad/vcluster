abfcdda1ff78a23676411debada01eb096d2e164
David Awad <davidawad64@gmail.com>
2015-12-14 04:31:09 -0500

    more polished tests

---
+++ test_cluster.py
@@ -0,0 +1,66 @@
+##
+# vcluster unit tests
+# @author David Awad
+#
+
+import unittest2 as unittest
+import virtual_machine
+import vcluster
+import yaml
+import json
+import sys
+import os
+
+
+class vclusterTests(unittest.TestCase):
+
+    def setUp(self):
+        self.config = vcluster.open_config('settings.yaml')
+
+    def test_yaml_config(self):
+        config = vcluster.open_config('settings.yaml')
+        self.assertEquals(config['command'], 'python unittest.py')
+        self.assertTrue(config['command'])
+
+    def test_json_config(self):
+        # FIXME JSON support is actually totally untested.
+        config = vcluster.open_config('sample.json')
+        # self.assertEquals(config['command'], 'python unittest.py')
+        # self.assertTrue(config['command'])
+
+    def test_config_ftype(self):
+        with open('settings.csv', 'w') as f:
+            f.write("hello there, I'm not a config file")
+        config = vcluster.open_config('settings.csv')
+        os.remove('settings.csv')
+        self.assertFalse(config)
+
+    def test_check_vagrantfiles(self):
+        """
+        make a temp_cluster folder, scrape it for clarity
+        """
+        vm_list = vcluster.generate_machines(self.config)
+        for item in vm_list:
+            self.assertIsInstance(item, virtual_machine.vm)
+
+    def test_render_clusters(self):
+        """
+        create vagrant files for a vcluster,
+        make sure templates render correctly
+        """
+        return  # TODO double check this!
+
+    def test_clear_vm(self):
+        """
+        make a temp_cluster folder, remove it
+        """
+        vm_list = vcluster.generate_machines(self.config)
+        self.assertTrue(vcluster.clear_vms())
+
+    def tearDown(self):
+        vcluster.clear_vms()
+        os.remove('settings.csv')
+        return
+
+if __name__ == '__main__':
+    unittest.main()
