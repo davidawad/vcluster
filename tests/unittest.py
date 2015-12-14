9144c49d7d04d72a7bd663277944146d5d667ec0
David Awad <davidawad64@gmail.com>
2015-12-13 23:01:33 -0500

    revisions of test suite

--- tests/unittest.py
+++
@@ -1,6 +0,0 @@
-import time
-
-
-for i in range(8):
-    print 'doing work...'
-    time.sleep(1)
---
+++ unittest.py
@@ -0,0 +1,41 @@
+import unittest2 as unittest
+import yaml
+import json
+import sys
+import os
+# append system path in order to import vcluster as a module
+sys.path.append(os.path.abspath('../'))
+import vcluster
+
+
+class vclusterTestCase(unittest.TestCase):
+
+    # read in a config yaml,
+    def test_yaml_config(self):
+        self.
+
+    # do the same for json
+    def test_json_config(self):
+        self.
+
+
+    # make a temp_cluster folder, remove it
+    def double_check_folder_kill(self)
+    def test(self):
+        self.os.(fun(3), 4)
+
+    def test_render_clusters(self):
+        """
+        create vagrant files for a vcluster,
+        make sure templates render correctly
+        """
+        return
+
+    def test(self):
+        self.assertEqual(fun(3), 4)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
